75493d4527923bfd99c81ef107160d0fc8b21f90
Hefsa Afzaal <hefsaafzaal18@gmail.com>
2025-12-08 03:28:01 -0800

    Add Navbar

--- src/Web-Page/header.jsx
+++ src/Web-Page/header.jsx
@@ -1,11 +1,34 @@
-import React from "react";
-import Bgimg from "./Img/header.png"; // make sure extension is correct
+import React from "react"; // make sure extension is correct
+import Bg1 from './Img/header.png'
+import Bg2 from './Img/header2.jpg'
+import Bg3 from './Img/header3.jpg'
+
 
 function Header() {
   return (
-    <div>
-      <img src={Bgimg} className="img-fluid" alt="Header" />
+  <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active" data-bs-interval="10000">
+      <img src={Bg1} class="d-block w-100" alt="..." />
     </div>
+    <div class="carousel-item" data-bs-interval="2000">
+      <img src={Bg2} class="d-block w-100" alt="..." />
+    </div>
+    <div class="carousel-item">
+      <img src={Bg3} class="d-block w-100" alt="..." />
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+    
+  
   );
 }
 
